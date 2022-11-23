dcc2312b59dd2edb6726e60f383b7c9d0662f669
wasifh15 <88007788+wasifh15@users.noreply.github.com>
2022-11-22 22:19:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function onPlayerMessage(event) {
                 // Get current clip label
                 var currentClipLabel = message.value.label;
                 // If this is one of the "last" clips
-                if (currentClipLabel === itm-feheren-16sec-13-06q1-1 || currentClipLabel === itm-feketen-16sec-13-ax9w-1) {
+                if (currentClipLabel === itm-feheren-16sec-13-06q1 || currentClipLabel === itm-feketen-16sec-13-ax9w) {
                     // Get Audio element for background music
                     var musicPlayer = document.getElementById('music-player');
                     // Pause background music
