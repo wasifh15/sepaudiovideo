503e9a58806859f21c7dccde91ee15dec08e333e
wasifh15 <88007788+wasifh15@users.noreply.github.com>
2022-11-22 22:22:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Labels of "last" clips
-var lightEndClipLabel = 'Scene_05_White_ONLY';
-var darkEndClipLabel = 'Scene_06_END';
+var lightEndClipLabel = 'S13T1 FEHEREN';
+var darkEndClipLabel = 'S13T1 FEKETEN';
 
 function startPlayback(clickShield) {
     // Remove shield element
@@ -30,7 +30,7 @@ function onPlayerMessage(event) {
                 // Get current clip label
                 var currentClipLabel = message.value.label;
                 // If this is one of the "last" clips
-                if (currentClipLabel === itm-feheren-16sec-13-06q1 || currentClipLabel === itm-feketen-16sec-13-ax9w) {
+                if (currentClipLabel === lightEndClipLabel || currentClipLabel === darkEndClipLabel) {
                     // Get Audio element for background music
                     var musicPlayer = document.getElementById('music-player');
                     // Pause background music
